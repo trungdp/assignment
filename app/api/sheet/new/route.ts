5580e67b6e89d666d3396eefe2c6cca0119e163c
trungdp <trungtrs1998@gmail.com>
2024-03-14 23:01:07 +0700

    feat: remove get api

--- app/api/sheet/new/route.ts
+++ app/api/sheet/new/route.ts
@@ -1,15 +1,5 @@
 import { createNewSpreadsheetDocument } from "@/libs/google-sheet";
 
-export async function GET() {
-  try {
-    const docId = await createNewSpreadsheetDocument();
-
-    return Response.json({ ok: true, data: { docId } });
-  } catch (error) {
-    return Response.json({ ok: false, error: "Something wrong!" });
-  }
-}
-
 export async function POST() {
   try {
     const docId = await createNewSpreadsheetDocument();
